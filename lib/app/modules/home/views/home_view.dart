1af65977d0c1914e8275ddeff3467c71275cda25
Hellrik321 <ph.lo3321@gmail.com>
2022-02-04 23:46:45 -0300

    Campos de texto Login

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -4,18 +4,37 @@ import 'package:get/get.dart';
 
 import '../controllers/home_controller.dart';
 
+import '../../../utils/colorTheme.dart';
+
 class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('HomeView'),
-        centerTitle: true,
-      ),
+      backgroundColor: backGround,
       body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              color: textFieldGray,
+              height: 50,
+              child: TextField(),
+            ),
+            Container(
+              color: textFieldGray,
+              height: 50,
+              child: TextField(),
+            ),
+            Container(
+              color: button,
+              height: 50,
+              child: MaterialButton(
+                onPressed: null,
+                child: Text("Entrar"),
+              ),
+            ),
+          ],
         ),
       ),
     );
---
+++ lib/app/utils/colorTheme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const textFieldGray = Color.fromARGB(255, 241, 241, 241);
+const backGround = Color.fromARGB(255, 253, 253, 253);
+const button = Color.fromARGB(255, 164, 228, 137);
+const iconColor = Color.fromARGB(255, 121, 121, 121);
