34802f86d208a38fa5dc7de5499de12d9f99018a
Alex Wendel <alexwendel11@gmail.com>
2022-02-05 20:08:11 -0300

    Alterações no tamanho e espaçamento dos elementos

--- lib/app/modules/signIn/views/sign_in_view.dart
+++ lib/app/modules/signIn/views/sign_in_view.dart
@@ -10,38 +10,44 @@ import '../controllers/sign_in_controller.dart';
 class SignInView extends GetResponsiveView<SingInController> {
   @override
   Widget build(BuildContext context) {
+    print(context.textScaleFactor);
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        toolbarHeight: 70,
-        title: Text("Crie sua conta", style: appbartext),
-        backgroundColor: backGround,
-      ),
       body: ListView(
         padding: EdgeInsets.symmetric(horizontal: 20),
         children: [
-          Divider(height: 10, color: iconGray),
           Container(
-            alignment: Alignment.centerLeft,
-            padding: EdgeInsets.only(left: 20, right: 20, top: 10, bottom: 20),
-            // height: 50,
-            width: double.maxFinite,
-            child: Text(
-              "Preencha os campos abaixo para criar sua conta",
-              style: TextStyle(fontWeight: FontWeight.w500, fontSize: 30),
-            ),
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.only(top: 30, bottom: 20, left: 5, right: 5),
+              // height: 50,
+              width: double.maxFinite,
+              child: Column(children: [
+                Text("Preencha os campos abaixo para criar sua conta",
+                    style:
+                        TextStyle(fontWeight: FontWeight.w500, fontSize: 30)),
+                Divider(height: 15 * context.textScaleFactor, color: cardGray),
+              ])),
+          InputText(
+              hintText: "Nome completo",
+              icon: Icon(Icons.person_outline_rounded)),
+          SizedBox(height: 10),
+          InputText(
+              hintText: "Digite o e-mail", icon: Icon(Icons.email_outlined)),
+          SizedBox(height: 2),
+          InputText(
+              hintText: "Confirme o email", icon: Icon(Icons.email_outlined)),
+          SizedBox(height: 10),
+          InputText(
+            hintText: "Digite a senha",
+            obfuscatedField: true,
+            icon: Icon(Icons.lock_outline),
           ),
-          Divider(height: 20, color: Colors.transparent),
-          InputText(hintText: "Nome completo", icon: Icon(Icons.person)),
-          Divider(height: 20, color: Colors.transparent),
-          InputText(hintText: "Digite o e-mail", icon: Icon(Icons.email)),
-          Divider(height: 20, color: Colors.transparent),
-          InputText(hintText: "Confirme o email", icon: Icon(Icons.email)),
-          Divider(height: 20, color: Colors.transparent),
-          InputText(hintText: "Digite a senha"),
-          Divider(height: 20, color: Colors.transparent),
-          InputText(hintText: "Confirme a senha"),
-          Divider(height: 50, color: Colors.transparent),
+          SizedBox(height: 2),
+          InputText(
+            hintText: "Confirme a senha",
+            obfuscatedField: true,
+            icon: Icon(Icons.lock_outline),
+          ),
+          SizedBox(height: 10),
           Button(
             text: "Criar conta",
             onPressed: () {},
