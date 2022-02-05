6aa19526b2a183590e3e33068cb6c341053a2ac4
Alex Wendel <alexwendel11@gmail.com>
2022-02-05 20:15:48 -0300

    Consertando código quebrado

--- lib/app/models/loginform.dart
+++ lib/app/models/loginform.dart
@@ -3,5 +3,5 @@ import 'package:get/get.dart';
 class LoginForm {
   final String email;
   final String password;
-  LoginForm({this.email="", this.password=""})
+  LoginForm({this.email = "", this.password = ""});
 }
