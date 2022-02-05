69215a30b67cff974758146fd0195fbd49858ed0
Alex Wendel <alexwendel11@gmail.com>
2022-02-05 20:12:47 -0300

    Alteração para formato genérico de formulário

--- lib/app/models/loginform.dart
+++ lib/app/models/loginform.dart
@@ -1,16 +1,7 @@
 import 'package:get/get.dart';
 
 class LoginForm {
-  LoginForm({required String email, required String password}) {
-    this.email = email;
-    this.password = password;
-  }
-
-  late Rx<String> _email;
-  set email(String value) => _email.value = value;
-  String get email => _email.value;
-
-  late Rx<String> _password;
-  set password(String value) => _password.value = value;
-  String get password => _password.value;
+  final String email;
+  final String password;
+  LoginForm({this.email="", this.password=""})
 }
