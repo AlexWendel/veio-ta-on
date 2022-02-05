ee5cd65b0dd21971dfca58e002de3c475cce159c
Yures1lva <yure.siilva9@gmail.com>
2022-02-05 06:15:36 -0300

    criando sing_in_view

--- lib/app/modules/signIn/views/sign_in_view.dart
+++ lib/app/modules/signIn/views/sign_in_view.dart
@@ -10,21 +10,127 @@ class SignInView extends GetView<SingInController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 0,
+        toolbarHeight: 70,
         title: Text("Crie sua conta", style: appbartext),
         backgroundColor: backGround,
       ),
-      body: Center(
-          child: ListView(
+      body: ListView(
         padding: EdgeInsets.only(left: 5, right: 5),
         children: [
+          Divider(height: 10, color: iconGray),
           Container(
-            child: TextField(),
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.only(left: 20, right: 20, top: 10, bottom: 20),
+            // height: 50,
+            width: double.maxFinite,
+            child: Text(
+              "Preencha os campos abaixo para criar sua conta",
+              style: TextStyle(fontWeight: FontWeight.w500, fontSize: 30),
+            ),
           ),
+          Divider(height: 20, color: Colors.transparent),
           Container(
-            child: TextField(),
-          )
+            height: 60,
+            padding: EdgeInsets.only(left: 20, right: 20),
+            alignment: Alignment.center,
+            child: TextField(
+              cursorColor: verdeBosta,
+              decoration: InputDecoration(
+                hintText: "Nome completo ",
+                hintStyle: TextStyle(fontSize: 20),
+                prefixIcon: Icon(
+                  Icons.person,
+                  size: 27,
+                ),
+              ),
+            ),
+          ),
+          Divider(height: 20, color: Colors.transparent),
+          Container(
+            height: 60,
+            padding: EdgeInsets.only(left: 20, right: 20),
+            alignment: Alignment.center,
+            child: TextField(
+              cursorColor: verdeBosta,
+              decoration: InputDecoration(
+                hintText: "Digite o email ",
+                hintStyle: TextStyle(fontSize: 20),
+                prefixIcon: Icon(
+                  Icons.email,
+                  size: 27,
+                ),
+              ),
+            ),
+          ),
+          Divider(height: 20, color: Colors.transparent),
+          Container(
+            height: 60,
+            padding: EdgeInsets.only(left: 20, right: 20),
+            alignment: Alignment.center,
+            child: TextField(
+              cursorColor: verdeBosta,
+              decoration: InputDecoration(
+                hintText: "Confirme o email ",
+                hintStyle: TextStyle(fontSize: 20),
+                prefixIcon: Icon(
+                  Icons.email,
+                  size: 27,
+                ),
+              ),
+            ),
+          ),
+          Divider(height: 20, color: Colors.transparent),
+          Container(
+            height: 60,
+            padding: EdgeInsets.only(left: 20, right: 20),
+            alignment: Alignment.center,
+            child: TextField(
+              cursorColor: verdeBosta,
+              decoration: InputDecoration(
+                hintText: "Digite a senha ",
+                hintStyle: TextStyle(fontSize: 20),
+                prefixIcon: Icon(
+                  Icons.lock,
+                  size: 27,
+                ),
+              ),
+            ),
+          ),
+          Divider(height: 20, color: Colors.transparent),
+          Container(
+            height: 60,
+            padding: EdgeInsets.only(left: 20, right: 20),
+            alignment: Alignment.center,
+            child: TextField(
+              cursorColor: verdeBosta,
+              decoration: InputDecoration(
+                hintText: "Confirme a senha ",
+                hintStyle: TextStyle(fontSize: 20),
+                prefixIcon: Icon(
+                  Icons.lock,
+                  size: 27,
+                ),
+              ),
+            ),
+          ),
+          Divider(height: 50, color: Colors.transparent),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 120),
+            height: 60,
+            width: 200,
+            child: ElevatedButton(
+                child: Text(
+                  "Criar conta",
+                  style: textonormal,
+                ),
+                onPressed: () {
+                  // Get.to(() => HomeView());
+                }),
+          ),
+          Divider(height: 30, color: Colors.transparent),
         ],
-      )),
+      ),
     );
   }
 }
