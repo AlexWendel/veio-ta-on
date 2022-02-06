80f74f6f37446d88d9991da9a2203567b2579a98
Alex Wendel <alexwendel11@gmail.com>
2022-02-05 23:36:00 -0300

    sectionHeaderFontSize and sectionSubheaderFontSize

--- lib/app/utils/common.sizes.dart
+++ lib/app/utils/common.sizes.dart
@@ -1,4 +1,5 @@
-const double sectionHeaderFontSize = 20;
+const double sectionHeaderFontSize = 18;
+const double sectionSubheaderFontSize = 16;
 const double mediumIconSize = 40;
 const double defaultFontSize = 20;
 const double defaultCardDescriptionSize = 18;
