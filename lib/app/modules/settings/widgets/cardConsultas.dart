26470e95be73363dd1e419ab84044f752c11850a
Alex Wendel <alexwendel11@gmail.com>
2022-02-06 19:07:22 -0300

    revamp no card de consultas

--- lib/app/modules/settings/widgets/cardConsultas.dart
+++ lib/app/modules/settings/widgets/cardConsultas.dart
@@ -1,106 +1,100 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:hospital_maraba/app/models/agendamento.dart';
+import 'package:hospital_maraba/app/modules/agendamentos/minhas_consultas/views/consulta_agendada.dart';
+import 'package:hospital_maraba/app/modules/agendamentos/views/check_in_view.dart';
 import 'package:hospital_maraba/app/utils/colorTheme.dart';
+import 'package:hospital_maraba/app/utils/common.colors.dart';
 import 'package:hospital_maraba/app/utils/common.sizes.dart';
 
 class CardConsultas extends GetResponsiveView {
-  final String text;
-  final String description;
-  final Icon icon;
-  final Function()? onTap;
-
-  CardConsultas(
-      {required this.text,
-      required this.description,
-      required this.icon,
-      required this.onTap});
+  final Agendamento agendamento;
+  CardConsultas({required this.agendamento});
 
   @override
   Widget build(BuildContext context) {
     return Ink(
         child: InkWell(
-      splashColor: verdeBosta,
-      onTap: onTap,
+      splashColor: Get.theme.splashColor,
+      onTap: () => Get.to(CheckInView(agendamento)),
       child: Container(
         alignment: Alignment.centerLeft,
         // height: 150,
-        margin: EdgeInsets.all(5),
-        padding: EdgeInsets.all(10),
+        margin: EdgeInsets.symmetric(vertical: 5),
+        padding: EdgeInsets.all(20),
         decoration: BoxDecoration(
           color: textFieldGray,
           borderRadius: BorderRadius.circular(8),
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              text,
-              style: TextStyle(
-                  color: Color.fromRGBO(120, 120, 120, 1),
-                  fontWeight: FontWeight.w600,
-                  fontSize: defaultFontSize / context.textScaleFactor),
-            ),
-
-            Container(
-              // width: 260,
-              padding: EdgeInsets.only(top: 8, left: 20),
-              child: Text(
-                "Hospital Municipal - Folha 17, Quadra Especial - Nova Marabá - PA",
+        child:
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                agendamento.especialidade,
                 style: TextStyle(
-                    color: Color.fromRGBO(160, 160, 160, 1),
-                    fontWeight: FontWeight.w400,
-                    fontSize:
-                        defaultCardDescriptionSize / context.textScaleFactor),
+                    color: headerGray,
+                    fontWeight: FontWeight.w600,
+                    fontSize: defaultFontSize / context.textScaleFactor),
               ),
-            ),
-            Container(
-              // width: 260,
-              padding: EdgeInsets.only(top: 8, left: 20),
-              child: Text(
-                "13/05/22 as 14:30h",
-                style: TextStyle(
-                    color: Color.fromRGBO(160, 160, 160, 1),
-                    fontWeight: FontWeight.w400,
-                    fontSize:
-                        defaultCardDescriptionSize / context.textScaleFactor),
+              SizedBox(height: 10),
+              RichText(
+                text: TextSpan(
+                  style: DefaultTextStyle.of(context).style,
+                  children: <TextSpan>[
+                    TextSpan(
+                        text: 'Médico: ',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black54)),
+                    TextSpan(
+                        text: agendamento.medico,
+                        style: TextStyle(
+                            color: headerGray, fontWeight: FontWeight.w400)),
+                  ],
+                ),
               ),
-            ),
-            Container(
-              // width: 260,
-              padding: EdgeInsets.only(top: 8, left: 20),
-              child: Text(
-                "Dr. Wanderley Andrade",
-                style: TextStyle(
-                    color: Color.fromRGBO(160, 160, 160, 1),
-                    fontWeight: FontWeight.w400,
-                    fontSize:
-                        defaultCardDescriptionSize / context.textScaleFactor),
+              RichText(
+                text: TextSpan(
+                  style: DefaultTextStyle.of(context).style,
+                  children: <TextSpan>[
+                    TextSpan(
+                        text: 'Local: ',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black54)),
+                    TextSpan(
+                        text: agendamento.local,
+                        style: TextStyle(
+                            color: headerGray, fontWeight: FontWeight.w400)),
+                  ],
+                ),
               ),
-            ),
-            // Container(
-            //   width: 260,
-            //   padding: EdgeInsets.only(top: 5, bottom: 5, left: 20),
-            //   child: Text(
-            //     "Clique para saber mais ",
-            //     style: TextStyle(
-            //         color: Color.fromRGBO(160, 160, 160, 1),
-            //         fontWeight: FontWeight.w400,
-            //         fontSize:
-            //             defaultCardDescriptionSize / context.textScaleFactor),
-            //   ),
-            // ),
-
-            // Text(
-            //   "Clique para saber mais ",
-            //   style: TextStyle(
-            //       fontSize:
-            //           sectionHeaderFontSize / context.textScaleFactor,
-            //       color: Colors.black54),
-            // )
-            // ),
-          ],
-        ),
+              RichText(
+                text: TextSpan(
+                  style: DefaultTextStyle.of(context).style,
+                  children: <TextSpan>[
+                    TextSpan(
+                        text: 'Horário: ',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black54)),
+                    TextSpan(
+                        text: "13 de Fevereiro de 2022 às 14:30h",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            color: Colors.black54)),
+                  ],
+                ),
+              )
+            ],
+          ),
+          IconButton(
+              icon: Icon(Icons.arrow_forward_ios, color: headerGray),
+              onPressed: () => Get.to(ConsultaAgendada()))
+        ]),
       ),
     ));
   }
