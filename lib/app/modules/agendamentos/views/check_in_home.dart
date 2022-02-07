22b45dca96eb3da4b0b1471fc7561a6af0dee43d
Yures1lva <yure.siilva9@gmail.com>
2022-02-06 23:20:27 -0300

    adicionando tela de home de checkin

---
+++ lib/app/modules/agendamentos/views/check_in_home.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:hospital_maraba/app/models/agendamento.dart';
+import 'package:hospital_maraba/app/modules/DefaultDesignScreen.dart';
+import 'package:hospital_maraba/app/modules/agendamentos/minhas_consultas/views/consulta_agendada.dart';
+import 'package:hospital_maraba/app/modules/agendamentos/views/resultado_final.dart';
+import 'package:hospital_maraba/app/modules/agendamentos/widgets/cardCheckin.dart';
+import 'package:hospital_maraba/app/modules/agendamentos/widgets/cardResultados.dart';
+import 'package:hospital_maraba/app/modules/settings/widgets/cardConsultas.dart';
+import 'package:hospital_maraba/app/utils/colorTheme.dart';
+import 'package:hospital_maraba/app/utils/common.sizes.dart';
+import 'package:hospital_maraba/app/widgets/TitleSliverAppBar.dart';
+import 'package:hospital_maraba/app/widgets/cardHome.dart';
+import 'package:hospital_maraba/app/widgets/modalScreen.dart';
+
+class CheckInHome extends GetView {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultDesignScreen(
+      sliverAppBar: TitleSliverAppBar(title: "Fazer checkin"),
+      body: Column(children: [
+        Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                "Realizar checkin",
+                style: Get.theme.textTheme.displaySmall?.copyWith(
+                    color: Colors.black54, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 5),
+              Text(
+                "Selecione uma consulta para fazer o checkin",
+                style: Get.theme.textTheme.headline5
+                    ?.copyWith(color: Colors.black54),
+              )
+            ]),
+        SizedBox(height: 8),
+        CardCheckin(
+            agendamento: Agendamento(
+                especialidade: "Ressonância Magnutica",
+                medico: "Dr.Iago Costa Batima da silva",
+                local: "Hospital Municipal de Marabá",
+                paciente: "Alex Wendel Oliveira da Silva",
+                protocolo: "111111111111111111111")),
+      ]),
+    );
+  }
+}
---
+++ lib/app/modules/agendamentos/widgets/cardCheckin.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:hospital_maraba/app/models/agendamento.dart';
+import 'package:hospital_maraba/app/modules/agendamentos/minhas_consultas/views/consulta_agendada.dart';
+import 'package:hospital_maraba/app/modules/agendamentos/views/check_in_view.dart';
+import 'package:hospital_maraba/app/modules/agendamentos/views/resultado_final.dart';
+import 'package:hospital_maraba/app/utils/colorTheme.dart';
+import 'package:hospital_maraba/app/utils/common.colors.dart';
+import 'package:hospital_maraba/app/utils/common.sizes.dart';
+
+class CardCheckin extends GetView {
+  final Agendamento agendamento;
+  CardCheckin({required this.agendamento});
+
+  @override
+  Widget build(BuildContext context) {
+    return Ink(
+        child: InkWell(
+      splashColor: Get.theme.splashColor,
+      onTap: () => Get.to(CheckInView(agendamento)),
+      child: Container(
+        alignment: Alignment.centerLeft,
+        // height: 150,
+        margin: EdgeInsets.symmetric(vertical: 5),
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: textFieldGray,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child:
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                agendamento.especialidade,
+                style: TextStyle(
+                    color: headerGray,
+                    fontWeight: FontWeight.w600,
+                    fontSize: defaultFontSize / context.textScaleFactor),
+              ),
+              SizedBox(height: 10),
+              RichText(
+                text: TextSpan(
+                  style: DefaultTextStyle.of(context).style,
+                  children: <TextSpan>[
+                    TextSpan(
+                        text: 'Médico: ',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black54)),
+                    TextSpan(
+                        text: agendamento.medico,
+                        style: TextStyle(
+                            color: headerGray, fontWeight: FontWeight.w400)),
+                  ],
+                ),
+              ),
+              RichText(
+                text: TextSpan(
+                  style: DefaultTextStyle.of(context).style,
+                  children: <TextSpan>[
+                    TextSpan(
+                        text: 'Local: ',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black54)),
+                    TextSpan(
+                        text: agendamento.local,
+                        style: TextStyle(
+                            color: headerGray, fontWeight: FontWeight.w400)),
+                  ],
+                ),
+              ),
+              RichText(
+                text: TextSpan(
+                  style: DefaultTextStyle.of(context).style,
+                  children: <TextSpan>[
+                    TextSpan(
+                        text: 'Horário: ',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black54)),
+                    TextSpan(
+                        text: "13 de Fevereiro de 2022 às 14:30h",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            color: Colors.black54)),
+                  ],
+                ),
+              )
+            ],
+          ),
+          IconButton(
+              icon: Icon(Icons.arrow_forward_ios, color: headerGray),
+              onPressed: () {})
+        ]),
+      ),
+    ));
+  }
+}
