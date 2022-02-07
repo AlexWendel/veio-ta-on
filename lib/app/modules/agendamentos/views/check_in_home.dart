76b8dda86e38847f1e45eb2158ef0f27eaca7411
Alex Wendel <alexwendel11@gmail.com>
2022-02-07 09:28:08 -0300

    Implementando home de login dinâmica

--- lib/app/modules/agendamentos/views/check_in_home.dart
+++ lib/app/modules/agendamentos/views/check_in_home.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:hospital_maraba/app/authcontrolller.dart';
 import 'package:hospital_maraba/app/models/agendamento.dart';
 import 'package:hospital_maraba/app/modules/DefaultDesignScreen.dart';
 import 'package:hospital_maraba/app/modules/agendamentos/widgets/cardCheckin.dart';
+import 'package:hospital_maraba/app/modules/home/controllers/home_controller.dart';
 import 'package:hospital_maraba/app/widgets/TitleSliverAppBar.dart';
 
 class CheckInHome extends GetView {
+  HomeController homeController = Get.put(HomeController());
+  List<Agendamento> defaultAgendamento = [
+    Agendamento(
+        data: "25/10/2022",
+        especialidade: "Ressonância Magnutica",
+        medico: "Dr.Iago Costa Batima da silva",
+        local: "Hospital Municipal de Marabá",
+        paciente: "Alex Wendel Oliveira da Silva",
+        protocolo: "111111111111111111111"),
+    Agendamento(
+        data: "25/10/2022",
+        especialidade: "Ressonância Magnética",
+        medico: "Dr. Josué Carvalho Sarazaro",
+        local: "Hospital Regional de Marabá",
+        paciente: "Alex Wendel Oliveira da Silva",
+        protocolo: "999999999999998"),
+    Agendamento(
+        data: "25/10/2022",
+        especialidade: "Tomografia Computadorizada",
+        medico: "Dr. Henrique Santos",
+        local: "Hospital Municipal de Marabá",
+        paciente: "Alex Wendel Oliveira da Silva",
+        protocolo: "9998999+955"),
+    Agendamento(
+        data: "25/10/2022",
+        especialidade: "Análise psiquiátrica",
+        medico: "Dr. Warley Rabelo Galvão",
+        local: "Hospital Municipal de Marabá",
+        paciente: "Alex Wendel Oliveira da Silva",
+        protocolo: "99999997699999989")
+  ];
+
   @override
   Widget build(BuildContext context) {
     return DefaultDesignScreen(
@@ -29,38 +63,36 @@ class CheckInHome extends GetView {
               )
             ]),
         SizedBox(height: 8),
-        CardCheckin(
-            agendamento: Agendamento(
-                data: "25/10/2022",
-                especialidade: "Ressonância Magnutica",
-                medico: "Dr.Iago Costa Batima da silva",
-                local: "Hospital Municipal de Marabá",
-                paciente: "Alex Wendel Oliveira da Silva",
-                protocolo: "111111111111111111111")),
-        CardCheckin(
-            agendamento: Agendamento(
-                data: "25/10/2022",
-                especialidade: "Ressonância Magnética",
-                medico: "Dr. Josué Carvalho Sarazaro",
-                local: "Hospital Regional de Marabá",
-                paciente: "Alex Wendel Oliveira da Silva",
-                protocolo: "999999999999998")),
-        CardCheckin(
-            agendamento: Agendamento(
-                data: "25/10/2022",
-                especialidade: "Tomografia Computadorizada",
-                medico: "Dr. Henrique Santos",
-                local: "Hospital Municipal de Marabá",
-                paciente: "Alex Wendel Oliveira da Silva",
-                protocolo: "9998999+955")),
-        CardCheckin(
-            agendamento: Agendamento(
-                data: "25/10/2022",
-                especialidade: "Análise psiquiátrica",
-                medico: "Dr. Warley Rabelo Galvão",
-                local: "Hospital Municipal de Marabá",
-                paciente: "Alex Wendel Oliveira da Silva",
-                protocolo: "99999997699999989")),
+        Obx(
+          () => Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.all(5),
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(8)),
+              child: ListView.builder(
+                  physics: NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: controller.agendamento.length + 3,
+                  itemBuilder: (context, index) {
+                    if (index <= 2) {
+                      return CardCheckin(
+                          agendamento: defaultAgendamento[index]);
+                    }
+
+                    return CardCheckin(
+                        agendamento: Agendamento(
+                            protocolo: "999999999999989",
+                            paciente:
+                                controller.agendamento[index - 3].paciente,
+                            local: controller.agendamento[index - 3].local,
+                            medico: controller.agendamento[index - 3].medico,
+                            especialidade:
+                                controller.agendamento[index - 3].especialidade,
+                            data: controller.agendamento[index - 3].data +
+                                " às " +
+                                controller.agendamento[index - 3].hora +
+                                "h"));
+                  })),
+        )
       ]),
     );
   }
