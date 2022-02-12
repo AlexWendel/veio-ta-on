5b66f38c463c1f6c888088709a06eaf8bd910809
Alex Wendel <alexwendel11@gmail.com>
2022-02-12 20:10:29 -0300

    Espaçamento entre título e cards

--- lib/app/modules/agendamentos/views/resultados_home.dart
+++ lib/app/modules/agendamentos/views/resultados_home.dart
@@ -3,15 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hospital_maraba/app/models/agendamento.dart';
 import 'package:hospital_maraba/app/modules/DefaultDesignScreen.dart';
-import 'package:hospital_maraba/app/modules/agendamentos/minhas_consultas/views/consulta_agendada.dart';
-import 'package:hospital_maraba/app/modules/agendamentos/views/resultado_final.dart';
 import 'package:hospital_maraba/app/modules/agendamentos/widgets/cardResultados.dart';
-import 'package:hospital_maraba/app/modules/settings/widgets/cardConsultas.dart';
-import 'package:hospital_maraba/app/utils/colorTheme.dart';
-import 'package:hospital_maraba/app/utils/common.sizes.dart';
 import 'package:hospital_maraba/app/widgets/TitleSliverAppBar.dart';
-import 'package:hospital_maraba/app/widgets/cardHome.dart';
-import 'package:hospital_maraba/app/widgets/modalScreen.dart';
 
 class ResultadosHome extends GetView {
   @override
@@ -25,17 +18,16 @@ class ResultadosHome extends GetView {
             children: [
               Text(
                 "Resultados de seus exames",
-                style: Get.theme.textTheme.displaySmall?.copyWith(
+                style: Get.theme.textTheme.headlineSmall?.copyWith(
                     color: Colors.black54, fontWeight: FontWeight.bold),
               ),
-              SizedBox(height: 5),
               Text(
                 "Toque para ver o resultado dos exames",
-                style: Get.theme.textTheme.headline5
+                style: Get.theme.textTheme.titleMedium
                     ?.copyWith(color: Colors.black54),
               )
             ]),
-        SizedBox(height: 8),
+        SizedBox(height: 20),
         CardResultados(
             agendamento: Agendamento(
                 data: "25/10/2022",
