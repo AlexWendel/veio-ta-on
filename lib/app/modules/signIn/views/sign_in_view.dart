f8201fa26c727ef4f75b82dc56c50757fd9caf23
Gust4Oliveira <gustavoolacerda@outlook.com.br>
2022-04-04 01:24:25 -0300

    Adição de máscaras para CPF, Telefone e SUSCard Remoção de Scaffold desnecessário Adição de atributos de SignIn

--- lib/app/modules/signIn/views/sign_in_view.dart
+++ lib/app/modules/signIn/views/sign_in_view.dart
@@ -11,51 +11,56 @@ class SignInView extends GetView<SignInController> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: ListView(
-        padding: EdgeInsets.symmetric(horizontal: 20),
-        children: [
-          Container(
-              alignment: Alignment.centerLeft,
-              padding: EdgeInsets.only(top: 30, bottom: 20, left: 5, right: 5),
-              // height: 50,
-              width: double.maxFinite,
-              child: Column(children: [
-                Text("Preencha os campos abaixo para criar sua conta",
-                    style: Get.theme.textTheme.displayMedium
-                        ?.copyWith(color: Colors.black54)),
-                SizedBox(height: 8),
-                Divider(
-                    height: 15 * context.textScaleFactor,
-                    color: cardGray.withOpacity(0.1)),
-              ])),
-          controller.nameInput.value,
-          SizedBox(height: 20),
-          controller.emailInput.value,
-          SizedBox(height: 2),
-          controller.emailConfirmInput.value,
-          SizedBox(height: 20),
-          controller.passwordInput.value,
-          SizedBox(height: 2),
-          controller.passwordConfirmInput.value,
-          SizedBox(height: 20),
-          ElevatedButton(
-            child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 20),
-                child: Text("Criar conta",
-                    style: Get.theme.textTheme.headline5?.copyWith(
-                        color: Get.theme.canvasColor,
-                        fontWeight: FontWeight.w600))),
-            onPressed: () {
-              print("TEAD");
-              controller.register();
-              // AuthController.instance.register(form);
-            },
-          ),
-          Divider(height: 30, color: Colors.transparent),
-        ],
-      ),
+    return ListView(
+      controller: ScrollController(),
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      children: [
+        Container(
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.only(top: 30, bottom: 20, left: 5, right: 5),
+            // height: 50,
+            width: double.maxFinite,
+            child: Column(children: [
+              Text("Preencha os campos abaixo para criar sua conta",
+                  style: Get.theme.textTheme.displayMedium
+                      ?.copyWith(color: Colors.black54)),
+              SizedBox(height: 8),
+              Divider(
+                  height: 15 * context.textScaleFactor,
+                  color: cardGray.withOpacity(0.1)),
+            ])),
+        controller.nameInput.value,
+        SizedBox(height: 2),
+        controller.cpfInput.value,
+        SizedBox(height: 2),
+        controller.rgInput.value,
+        SizedBox(height: 2),
+        controller.susCardInput.value,
+        SizedBox(height: 20),
+        controller.phoneInput.value,
+        SizedBox(height: 2),
+        controller.emailInput.value,
+        SizedBox(height: 2),
+        controller.emailConfirmInput.value,
+        SizedBox(height: 20),
+        controller.passwordInput.value,
+        SizedBox(height: 2),
+        controller.passwordConfirmInput.value,
+        SizedBox(height: 20),
+        ElevatedButton(
+          child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 20),
+              child: Text("Criar conta",
+                  style: Get.theme.textTheme.headline5?.copyWith(
+                      color: Get.theme.canvasColor,
+                      fontWeight: FontWeight.w600))),
+          onPressed: () {
+            controller.register();
+          },
+        ),
+        Divider(height: 30, color: Colors.transparent),
+      ],
+      // ),
     );
   }
 }
--- lib/app/widgets/input_text.dart
+++ lib/app/widgets/input_text.dart
@@ -1,7 +1,25 @@
 import 'package:hospital_maraba/app/utils/common.sizes.dart';
 
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+
+var maskPhone = MaskTextInputFormatter(
+    mask: '(##) #####-####',
+    filter: {"#": RegExp(r'[0-9]')},
+    type: MaskAutoCompletionType.lazy);
+var maskSusCard = MaskTextInputFormatter(
+    mask: '### #### #### ####',
+    filter: {"#": RegExp(r'[0-9]')},
+    type: MaskAutoCompletionType.lazy);
+var maskCPF = MaskTextInputFormatter(
+    mask: '###.###.###-##',
+    filter: {"#": RegExp(r'[0-9]')},
+    type: MaskAutoCompletionType.lazy);
+var maskRG = MaskTextInputFormatter(
+    mask: '#######',
+    filter: {"#": RegExp(r'[0-9]')},
+    type: MaskAutoCompletionType.lazy);
 
 class CustomInputText extends GetView {
   final List<String> texts = <String>[];
@@ -25,6 +43,7 @@ class CustomInputText extends GetView {
       height: 60,
       alignment: Alignment.center,
       child: TextField(
+        obscureText: obfuscatedField,
         onChanged: (value) {
           for (int i = 0; i < texts.length; i++) {
             if (texts[i].toLowerCase().contains(value.toLowerCase()) ||
@@ -36,8 +55,235 @@ class CustomInputText extends GetView {
           }
         },
         style: TextStyle(color: Colors.black87),
-        onEditingComplete: () =>
-            {}, // TODO: Verificar se o valor é igual em InputText que estiverem ligados
+        onEditingComplete: () => {},
+        controller: textFieldController.value,
+        decoration: InputDecoration(
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(color: Get.theme.primaryColor)),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          hintText: hintText,
+          hintStyle:
+              TextStyle(fontSize: defaultFontSize / context.textScaleFactor),
+          prefixIcon: Icon(
+            icon.icon,
+            size: 27,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CPFInputText extends GetView {
+  final List<String> texts = <String>[];
+  final itemHeight = <RxDouble>[];
+  final Function? onChanged;
+  final String hintText;
+  final Icon icon;
+  final bool obfuscatedField;
+  final Rx<TextEditingController> textFieldController =
+      TextEditingController().obs;
+
+  CPFInputText(
+      {this.onChanged,
+      required this.icon,
+      required this.hintText,
+      this.obfuscatedField = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      alignment: Alignment.center,
+      child: TextField(
+        inputFormatters: [maskCPF],
+        keyboardType: TextInputType.number,
+        onChanged: (value) {
+          for (int i = 0; i < texts.length; i++) {
+            if (texts[i].toLowerCase().contains(value.toLowerCase()) ||
+                value == "") {
+              itemHeight[i].value = 100;
+            } else {
+              itemHeight[i].value = 0;
+            }
+          }
+        },
+        style: TextStyle(color: Colors.black87),
+        onEditingComplete: () => {},
+        controller: textFieldController.value,
+        decoration: InputDecoration(
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(color: Get.theme.primaryColor)),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          hintText: hintText,
+          hintStyle:
+              TextStyle(fontSize: defaultFontSize / context.textScaleFactor),
+          prefixIcon: Icon(
+            icon.icon,
+            size: 27,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class RGInputText extends GetView {
+  final List<String> texts = <String>[];
+  final itemHeight = <RxDouble>[];
+  final Function? onChanged;
+  final String hintText;
+  final Icon icon;
+  final bool obfuscatedField;
+  final Rx<TextEditingController> textFieldController =
+      TextEditingController().obs;
+
+  RGInputText(
+      {this.onChanged,
+      required this.icon,
+      required this.hintText,
+      this.obfuscatedField = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      alignment: Alignment.center,
+      child: TextField(
+        inputFormatters: [maskRG],
+        keyboardType: TextInputType.number,
+        onChanged: (value) {
+          for (int i = 0; i < texts.length; i++) {
+            if (texts[i].toLowerCase().contains(value.toLowerCase()) ||
+                value == "") {
+              itemHeight[i].value = 100;
+            } else {
+              itemHeight[i].value = 0;
+            }
+          }
+        },
+        style: TextStyle(color: Colors.black87),
+        onEditingComplete: () => {},
+        controller: textFieldController.value,
+        decoration: InputDecoration(
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(color: Get.theme.primaryColor)),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          hintText: hintText,
+          hintStyle:
+              TextStyle(fontSize: defaultFontSize / context.textScaleFactor),
+          prefixIcon: Icon(
+            icon.icon,
+            size: 27,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SusCardInputText extends GetView {
+  final List<String> texts = <String>[];
+  final itemHeight = <RxDouble>[];
+  final Function? onChanged;
+  final String hintText;
+  final Icon icon;
+  final bool obfuscatedField;
+  final Rx<TextEditingController> textFieldController =
+      TextEditingController().obs;
+
+  SusCardInputText(
+      {this.onChanged,
+      required this.icon,
+      required this.hintText,
+      this.obfuscatedField = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      alignment: Alignment.center,
+      child: TextField(
+        inputFormatters: [maskSusCard],
+        keyboardType: TextInputType.number,
+        onChanged: (value) {
+          for (int i = 0; i < texts.length; i++) {
+            if (texts[i].toLowerCase().contains(value.toLowerCase()) ||
+                value == "") {
+              itemHeight[i].value = 100;
+            } else {
+              itemHeight[i].value = 0;
+            }
+          }
+        },
+        style: TextStyle(color: Colors.black87),
+        onEditingComplete: () => {},
+        controller: textFieldController.value,
+        decoration: InputDecoration(
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(color: Get.theme.primaryColor)),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          hintText: hintText,
+          hintStyle:
+              TextStyle(fontSize: defaultFontSize / context.textScaleFactor),
+          prefixIcon: Icon(
+            icon.icon,
+            size: 27,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class PhoneInputText extends GetView {
+  final List<String> texts = <String>[];
+  final itemHeight = <RxDouble>[];
+  final Function? onChanged;
+  final String hintText;
+  final Icon icon;
+  final bool obfuscatedField;
+  final Rx<TextEditingController> textFieldController =
+      TextEditingController().obs;
+
+  PhoneInputText(
+      {this.onChanged,
+      required this.icon,
+      required this.hintText,
+      this.obfuscatedField = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      alignment: Alignment.center,
+      child: TextField(
+        inputFormatters: [maskPhone],
+        keyboardType: TextInputType.number,
+        onChanged: (value) {
+          for (int i = 0; i < texts.length; i++) {
+            if (texts[i].toLowerCase().contains(value.toLowerCase()) ||
+                value == "") {
+              itemHeight[i].value = 100;
+            } else {
+              itemHeight[i].value = 0;
+            }
+          }
+        },
+        style: TextStyle(color: Colors.black87),
+        onEditingComplete: () => {},
         controller: textFieldController.value,
         decoration: InputDecoration(
           focusedBorder: OutlineInputBorder(
