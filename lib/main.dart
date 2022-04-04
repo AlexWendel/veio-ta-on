156a3285f180ca2460d2de62230b2fc564b59c5e
Gust4Oliveira <gustavoolacerda@outlook.com.br>
2022-04-04 01:19:24 -0300

    Reconfiguração do controle de autenticação

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:get/get.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:hospital_maraba/app/auth_controller.dart';
 import 'firebase_options.dart';
 
 import 'app/modules/home/controllers/home_controller.dart';
@@ -15,6 +16,7 @@ Future<void> main() async {
       [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   Get.put(HomeController());
+  Get.put(AuthController());
   runApp(
     GetMaterialApp(
       title: "Agendamento Fácil",
