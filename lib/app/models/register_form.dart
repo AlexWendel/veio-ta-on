4fbf3dcb115261536fb3537b727b4c8dcdf4a9f0
Gust4Oliveira <gustavoolacerda@outlook.com.br>
2022-04-04 01:21:44 -0300

    Adição de atributos para SignIn

--- lib/app/models/register_form.dart
+++ lib/app/models/register_form.dart
@@ -1,6 +1,27 @@
 class RegisterForm {
-  RegisterForm({this.name = "", this.email = "", this.password = ""});
-  String email;
+  RegisterForm(
+      {this.name = "",
+      this.cpf = "",
+      this.rg = "",
+      this.susNumber = "",
+      this.email = "",
+      this.emailConfirmation = "",
+      this.phone = "",
+      this.password = "",
+      this.passwordConfirmation = ""});
+
   String name;
+  String cpf;
+  String rg;
+  String susNumber;
+  String phone;
+  String email;
+  String emailConfirmation;
   String password;
+  String passwordConfirmation;
+
+  @override
+  String toString() {
+    return 'RegisterForm: nome: ${name}\n cpf: ${cpf}\n rg: ${rg}\n susNumber: ${susNumber}\n phone: ${phone}\n email: ${email}\n emailConf: ${emailConfirmation}\n password: ${password}\n passwordConf: ${passwordConfirmation}';
+  }
 }
