26635ea32a03c33566c3f68262ec71eee9f3b2fb
Alex <alexwendel11@gmail.com>
2022-04-15 20:02:16 -0300

    telas e widgets da home adicionadas

---
+++ lib/app/modules/MainDesign.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class MainDesign extends GetView {
+  final List<Widget> body;
+  final SliverAppBar sliverAppBar;
+
+  MainDesign({
+    required this.body,
+    required this.sliverAppBar,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Get.theme.canvasColor,
+        body: CustomScrollView(slivers: [
+          sliverAppBar,
+          SliverList(delegate: SliverChildListDelegate(body)),
+          // SliverFillRemaining(
+          //   hasScrollBody: true,
+          //   child: Container(
+          //     margin: EdgeInsets.only(top: 20),
+          //     width: Get.width,
+          //     child: Padding(
+          //         padding: EdgeInsets.all(20),
+          //         child: Container(
+          //             width: Get.width, height: Get.height, child: body)),
+          //     decoration: BoxDecoration(
+          //       color: Get.theme.canvasColor,
+          //       borderRadius: BorderRadius.only(
+          //           topLeft: Radius.circular(25),
+          //           topRight: Radius.circular(25)),
+          //     ),
+          //   ),
+          // )
+        ]));
+  }
+}
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,35 +1,184 @@
 import 'package:flutter/material.dart';
-import 'package:hospital_maraba/app/auth_controller.dart';
-import 'package:flutter/rendering.dart';
-
 import 'package:get/get.dart';
+import 'package:hospital_maraba/app/modules/home/controllers/home_controller.dart';
+import 'package:hospital_maraba/app/modules/settings/views/settings_view.dart';
+import 'package:hospital_maraba/app/utils/common.sizes.dart';
+import 'package:hospital_maraba/app/modules/MainDesign.dart';
+import 'package:hospital_maraba/app/widgets/CardHome.dart';
 
-import '../controllers/home_controller.dart';
+class HomeSliverAppBar extends SliverAppBar {
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      toolbarHeight: 45 * context.textScaleFactor,
+      expandedHeight: 45 * context.textScaleFactor,
+      floating: true,
+      snap: true,
+      elevation: 0,
+      flexibleSpace: Container(
+        decoration: BoxDecoration(
+            color: Get.theme.backgroundColor,
+            border: Border(
+                bottom: BorderSide(
+              width: 0.2,
+            ))),
+      ),
+      shadowColor: Colors.transparent,
+      // leading: IconButton(
+      //     onPressed: () => Get.back(),
+      //     icon: Icon(backIcon, size: mediumIconSize)),
+      title: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(children: [
+            Flexible(
+              child: Text(
+                "Olá, ",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w700, /*color: Colors.black54*/
+                ),
+              ),
+            ),
+            Flexible(
+              child: Obx(
+                () => Text(
+                  "Indivíduo",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700, /*color: Colors.black54*/
+                  ),
+                ),
+              ),
+            ),
+          ]),
+          Row(children: [
+            // Icon(Icons.circle_rounded,
+            //     size: 14, color: Colors.lightGreenAccent),
+            Flexible(
+              child: Container(
+                alignment: Alignment.centerLeft,
+                width: Get.width / 2,
+                child: FittedBox(
+                  fit: BoxFit.scaleDown,
+                  child: RichText(
+                    text: TextSpan(
+                      text: 'Você tem ',
+                      style: TextStyle(
+                          fontSize: 14,
+                          /*color: Colors.black54*/
+                          fontWeight: FontWeight.w600),
+                      children: <TextSpan>[
+                        TextSpan(
+                            text: '4 ',
+                            style: Get.theme.textTheme.bodyMedium
+                                ?.copyWith(fontWeight: FontWeight.w800)),
+                        TextSpan(
+                            text: 'novas notificações!',
+                            style: Get.theme.textTheme.bodyMedium
+                                ?.copyWith(fontWeight: FontWeight.w600)),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ]),
+        ],
+      ),
+      actions: [
+        InkWell(
+          onTap: () {
+            // Get.to(() => PerfilView());
+          },
+          child: Container(
+            margin: EdgeInsets.only(top: 5, right: 5, bottom: 10),
+            width: 40,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage("imgs/cj.png"),
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
 
 class HomeView extends GetView<HomeController> {
   @override
+  final HomeController controller = Get.find<HomeController>();
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: Center(
-          child: ListView(
-        // padding: EdgeInsets.only(left: 20, right: 20),
-        controller: ScrollController(),
-        children: [
-          Text(
-            'HomeView is working',
-            style: TextStyle(fontSize: 20),
-          ),
-          ElevatedButton(
-              onPressed: () {
-                AuthController.instance.logout();
-              },
-              child: Text("Logout"))
-        ],
-      )),
-    );
+    List<Widget> itemList = [
+      Text("Faça agendamento e check-in de suas consultas",
+          textAlign: TextAlign.start,
+          style: Get.theme.textTheme.displaySmall?.copyWith(
+              fontSize: defaultFontSize / context.textScaleFactor,
+              color: Colors.black54,
+              fontWeight: FontWeight.w500)),
+      SizedBox(height: 20),
+      Wrap(runSpacing: 0, children: [
+        CardHome(
+            icon: Icon(Icons.note_alt_outlined, size: mediumIconSize),
+            onTap: () {
+              // Get.to(() => NovoAgendamentoView());
+            },
+            text: "Agendar consulta",
+            description: "Agende uma nova consulta"),
+        CardHome(
+            icon: Icon(Icons.check_box_outlined, size: mediumIconSize),
+            onTap: () {
+              // Get.to(CheckInHome());
+              // () => CheckInView(
+              // Agendamento(
+              //   especialidade: "Tratamento para Vício de Valorant",
+              //   medico: "Warley Rabelo Xeradão",
+              //   paciente: "Paulinho Paixão Bacana Pereira",
+              //   local: "Hospício Municipal de Marabá",
+              //   protocolo: "9999999999999999"))
+              //   );
+            },
+            text: "Fazer check-in",
+            description: "Gerar QR Code de uma consulta"),
+        // CardHome(
+        //     icon: Icon(Icons.notes_outlined,
+        //         size: mediumIconSize, color: iconGray),
+        //     onTap: () {
+        //       Get.to(() => ResultadosHome());
+        //     },
+        //     text: "Resultados",
+        //     description: "Resultados dos seus exames"),
+        CardHome(
+            icon: Icon(Icons.calendar_view_week, size: mediumIconSize),
+            onTap: () {
+              // Get.to(() => MinhasConsultasScreen());
+            },
+            text: "Minhas consultas",
+            description: "Veja suas consultas agendadas"),
+
+        CardHome(
+            icon:
+                Icon(Icons.circle_notifications_outlined, size: mediumIconSize),
+            onTap: () {
+              // Get.to(() => SemExames());
+            },
+            text: "Notificações",
+            description: "Notificações do aplicativo"),
+        CardHome(
+            icon: Icon(Icons.settings_outlined, size: mediumIconSize),
+            onTap: () {
+              Get.to(() => SettingsView());
+            },
+            text: "Configurações",
+            description: "Configurações do seu aplicativo"),
+      ])
+    ];
+
+    return MainDesign(body: itemList, sliverAppBar: HomeSliverAppBar());
   }
 }
