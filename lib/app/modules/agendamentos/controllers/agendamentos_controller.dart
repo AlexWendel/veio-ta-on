17ab386cca1e37513300595e189a181b36f72b8e
Hellrik321 <ph.lo3321@gmail.com>
2022-04-16 16:24:09 -0300

    limpando controlerr de agendamentos

--- lib/app/modules/agendamentos/controllers/agendamentos_controller.dart
+++ lib/app/modules/agendamentos/controllers/agendamentos_controller.dart
@@ -7,32 +7,8 @@ import 'package:hospital_maraba/app/models/paciente.dart';
 
 class AgendamentosController extends GetxController {
   final count = 0.obs;
-  Agendamento agendarConsulta(
-      Paciente pacienteRef, Local localRef, DateTime agendadoEm) {
-// [Consulta]
-// - id_consulta (unique_id)
-// - paciente (pacienteRef)
-// - medico (medicoRef)
-// - agendada_por (usuarioRef)
-// - agendado_em (datetime)
-// - local (localRef)
-// - horario (datetime)
-// - fez_checkin (bool)
-// - checkin_por (gerenciadorRef)
-// - fez_checkin_em (datetime)
-// - confirmacao (bool)
-// - confirmado_em (datetime)
-    // Chamar o firebase pra criar o document
-    return Agendamento();
-  }
 
-  Agendamento cancelarConsulta(
-      Paciente pacinteRef, Agendamento agendamentoRef) {
-    // Checar se o agendamento existe
-    // Verificar se o agendamento ainda está no período válido de cancelamento
-    // Criar o documento no firebase
-    // Retornar a referencia ao documento do firebase
-  }
+  //
 
   @override
   void onInit() {
