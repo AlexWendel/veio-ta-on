dc38b7904d114fa136fc23436e1b640179c2d2a1
Hellrik321 <ph.lo3321@gmail.com>
2022-04-16 19:17:11 -0300

    removendo importações

--- lib/app/models/agendamento.dart
+++ lib/app/models/agendamento.dart
@@ -1,3 +1,8 @@
+import 'package:hospital_maraba/app/models/exame.dart';
+import 'package:hospital_maraba/app/models/local.dart';
+
+import 'data.dart';
+
 class Agendamento {
 // ''[Consulta]
 // - id_consulta (unique_id)
@@ -13,4 +18,9 @@ class Agendamento {
 // - confirmacao (bool)
 // - confirmado_em (datetime)''
 
+  Data data;
+  Local local;
+  Exame exame;
+
+  Agendamento({required this.data, required this.local, required this.exame});
 }
