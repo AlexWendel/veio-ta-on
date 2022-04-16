b65d52d15542bd985c88282ef3c0c0d3d9c7099e
Hellrik321 <ph.lo3321@gmail.com>
2022-04-16 15:38:16 -0300

    Atualizando RadioBox

--- lib/app/widgets/radioBox.dart
+++ lib/app/widgets/radioBox.dart
@@ -4,30 +4,44 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
 import 'package:hospital_maraba/app/widgets/radioButton.dart';
 
-class RadioBoxController extends GetxController {}
-
 class RadioBox extends GetView {
+  List locais = ["1", "2", "3"];
   @override
   Widget build(BuildContext context) {
     List<Widget> radioList = [];
+    RxList<bool> selectionList = RxList<bool>();
+    RxList<Color> colorList = RxList<Color>();
+    for (int i = 0; i < locais.length; i++) {
+      selectionList.add(false);
+      colorList.add(Get.theme.highlightColor);
+      radioList.add(Obx(() => RadioButton(
+          color: colorList[i],
+          onTap: () {
+            selectionList[i] = true;
+            colorList[i] = Colors.green;
+            for (int j = 0; j < selectionList.length; j++) {
+              if (j != i) {
+                selectionList[j] = false;
+                colorList[j] = Get.theme.highlightColor;
+              }
+            }
+          },
+          text: "teste",
+          description: "testando",
+          icon: Icon(Icons.abc),
+          height: 100,
+          width: context.width)));
+    }
+
     return Container(
       height: 600,
       width: 200,
       color: Colors.green,
       child: ListView.builder(
         itemBuilder: (BuildContext context, index) {
-          radioList.add(RadioButton(
-            onTap: () => {},
-            text: "Teste",
-            description: "Testando",
-            icon: Icon(Icons.access_alarms),
-            height: 100,
-            width: context.width,
-            id: index,
-          ));
           return radioList[index];
         },
-        itemCount: 5,
+        itemCount: radioList.length,
       ),
     );
   }
--- lib/app/widgets/radioButton.dart
+++ lib/app/widgets/radioButton.dart
@@ -1,103 +1,88 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hospital_maraba/app/utils/common.sizes.dart';
+import 'package:hospital_maraba/app/widgets/radioBox.dart';
 
 class RadioButton extends GetResponsiveView {
-  final _isSelected = false.obs;
-  final color = Get.theme.highlightColor.obs;
+  final Color color;
   final Function() onTap;
   final String text;
   final String description;
   final Icon icon;
   final double width;
   final double height;
-  final int id;
+  int _id = 0;
 
-  RadioButton(
-      {required this.onTap,
-      required this.text,
-      required this.description,
-      required this.icon,
-      required this.height,
-      required this.width,
-      required this.id});
-
-  switchSelection() {
-    _isSelected.value = !_isSelected.value;
-  }
-
-  set onTap(Function onTap) {
-    this.onTap = onTap;
-  }
+  RadioButton({
+    required this.color,
+    required this.onTap,
+    required this.text,
+    required this.description,
+    required this.icon,
+    required this.height,
+    required this.width,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Ink(
       child: InkWell(
         splashColor: Get.theme.splashColor,
-        onTap: () {
-          if (_isSelected.value) {
-            color.value = Colors.green;
-          }
-
-          onTap();
-        },
-        child: Obx(
-          () => Container(
-            width: width,
-            alignment: Alignment.center,
-            height: height,
-            margin: EdgeInsets.all(10),
-            padding: EdgeInsets.all(5),
-            decoration: BoxDecoration(color: color.value),
-            child: Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 8),
-                  child: icon,
-                ),
-                VerticalDivider(
-                  width: 10,
-                  color: Colors.transparent,
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      alignment: Alignment.centerLeft,
-                      width: context.width / 2,
-                      child: FittedBox(
-                        fit: BoxFit.scaleDown,
-                        child: Text(
-                          text,
-                          style: TextStyle(
-                              fontWeight: FontWeight.w600,
-                              fontSize:
-                                  defaultFontSize / context.textScaleFactor),
-                        ),
+        onTap: onTap,
+        child: Container(
+          width: width,
+          alignment: Alignment.center,
+          height: height,
+          margin: EdgeInsets.all(10),
+          padding: EdgeInsets.all(5),
+          decoration: BoxDecoration(color: color),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 8),
+                child: icon,
+              ),
+              VerticalDivider(
+                width: 10,
+                color: Colors.transparent,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    width: context.width / 2,
+                    child: FittedBox(
+                      fit: BoxFit.scaleDown,
+                      child: Text(
+                        text,
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize:
+                                defaultFontSize / context.textScaleFactor),
                       ),
                     ),
-                    Container(
-                      alignment: Alignment.centerLeft,
-                      width: context.width / 2,
-                      padding: EdgeInsets.only(top: 5, bottom: 10),
-                      child: FittedBox(
-                        fit: BoxFit.scaleDown,
-                        child: Text(
-                          description,
-                          maxLines: 1,
-                          style: TextStyle(
-                              fontWeight: FontWeight.w400,
-                              fontSize: defaultCardDescriptionSize /
-                                  context.textScaleFactor),
-                        ),
+                  ),
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    width: context.width / 2,
+                    padding: EdgeInsets.only(top: 5, bottom: 10),
+                    child: FittedBox(
+                      fit: BoxFit.scaleDown,
+                      child: Text(
+                        description,
+                        maxLines: 1,
+                        style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontSize: defaultCardDescriptionSize /
+                                context.textScaleFactor),
                       ),
-                    )
-                  ],
-                )
-              ],
-            ),
+                    ),
+                  )
+                ],
+              )
+            ],
           ),
         ),
       ),
