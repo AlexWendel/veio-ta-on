ebcf8dd9a733e6133780602f02d3c93362e0800f
Alex Wendel <alexwendel11@gmail.com>
2022-04-23 14:26:28 -0300

    funções diversas implementadas

--- lib/app/data/database.dart
+++ lib/app/data/database.dart
@@ -1,52 +1,147 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get/get.dart';
 import 'package:hospital_maraba/app/models/agendamento.dart';
 import 'package:hospital_maraba/app/models/especialidade.dart';
 import 'package:hospital_maraba/app/models/local.dart';
-import '../models/user.dart';
+import 'package:hospital_maraba/app/models/user.dart';
 
-Future<UserLocal> getUserFromFirestore(String? uid) async {
-  // await FirebaseFirestore.instance.disableNetwork(); //Para testar offline
-  final userRef =
-      FirebaseFirestore.instance.collection('userDataStorage').withConverter(
-          fromFirestore: (snapshot, _) {
-            print("User ID: " + snapshot.id);
-            return UserLocal.fromJson(snapshot.data()!, snapshot.id);
-          },
+const userCollection = "userDataStorage";
+const agendamentosCollection = "agendamento";
+const locaisCollection = "local";
+const especialidadesCollection = "especialidades";
+const horariosCollection = "horarios_agendamentos";
+
+class DatabaseService extends GetxService {
+  final CollectionReference userDataCollectionRef =
+      FirebaseFirestore.instance.collection(userCollection);
+
+  final CollectionReference<UserLocal> userConverter =
+      FirebaseFirestore.instance.collection(userCollection).withConverter(
+          fromFirestore: (snapshot, _) =>
+              UserLocal.fromJson(snapshot.data()!, snapshot.id),
           toFirestore: (UserLocal user, _) => user.toJson());
-  return userRef.doc(uid).get().then((snapshot) {
-    snapshot.metadata.isFromCache
-        ? print("Usando cache local")
-        : print("Usando informações do FireStore");
-    return snapshot.data()!;
-  });
-}
 
-// TODO: Inserir dados no database e testar se está funcionando
-// TODO: Permitir também buscar diretamente por cartão do SUS, CPF, Nome ou RG
-Future<List<Agendamento>> getAllAgendamentosFromPaciente(
-    String pacienteID) async {
-  return [];
-}
+  final CollectionReference agendamentosCollectionRef =
+      FirebaseFirestore.instance.collection(agendamentosCollection);
 
-// TODO : Implementar listagem todas as especialidades
-Future<List<Especialidade>> getAllEspecialidades() async {
-  var especialidadesRef =
-      FirebaseFirestore.instance.collection("especialidade");
-  var especialidades = especialidadesRef
-      .get()
-      .then((snapshot) => List<Especialidade>.from(snapshot.docs));
-  return especialidades;
-}
+  final CollectionReference<Agendamento> agendamentosConverter =
+      FirebaseFirestore.instance
+          .collection(agendamentosCollection)
+          .withConverter(
+              fromFirestore: (snapshot, _) {
+                Map<String, dynamic> data = snapshot.data()!;
+                data.putIfAbsent("id", () => snapshot.id);
+                return Agendamento.fromJson(data);
+              },
+              toFirestore: (Agendamento agendamento, _) =>
+                  agendamento.toJson());
 
-// TODO : Implementar listagem de todos os locais
-Future<List<Especialidade>> getAllLocais() async {
-  return [];
-}
+  final CollectionReference locaisCollectionRef =
+      FirebaseFirestore.instance.collection(locaisCollection);
 
-//TODO : Criar agendamento e inserir no banco de dados...
-void createAgendamento(Agendamento formAgendamento) async {}
+  final CollectionReference<Local> locaisConverter =
+      FirebaseFirestore.instance.collection(locaisCollection).withConverter(
+          fromFirestore: (snapshot, options) {
+            Map<String, dynamic>? data = snapshot.data();
+            // Adicionando ID aos dados que vão para o model;
+            data!.putIfAbsent("id", () => snapshot.id);
+            return Local.fromJson(data);
+          },
+          toFirestore: (Local local, _) => local.toJson());
 
-//TODO Fazer...
-Future<List<Local>> getLocalFromFirestore() async {
-  return [];
+  final CollectionReference especialidadesCollectionRef =
+      FirebaseFirestore.instance.collection(especialidadesCollection);
+
+  final CollectionReference<Especialidade> especialidadesConverter =
+      FirebaseFirestore.instance
+          .collection(especialidadesCollection)
+          .withConverter(
+              fromFirestore: (snapshot, options) {
+                Map<String, dynamic> data = snapshot.data()!;
+                data.putIfAbsent("id", () => snapshot.id);
+                return Especialidade.fromJson(data);
+              },
+              toFirestore: (Especialidade especialidade, _) =>
+                  especialidade.toJson());
+
+  final CollectionReference datasAgendamentosCollectionRef =
+      FirebaseFirestore.instance.collection(horariosCollection);
+
+  Future<UserLocal> getUserFromFirestore(String? uid) async {
+    return userConverter.doc(uid).get().then((snapshot) {
+      snapshot.metadata.isFromCache
+          ? print("Usando cache local")
+          : print("Usando informações do FireStore");
+      return snapshot.data()!;
+    });
+  }
+
+  // TODO: Permitir também buscar diretamente por cartão do SUS, CPF, Nome ou RG
+  Future<List<Agendamento>> getAllAgendamentosFromPaciente(
+      String pacienteID) async {
+    DocumentReference pacienteRef = userDataCollectionRef.doc(pacienteID);
+
+    return agendamentosConverter
+        .where("paciente", isEqualTo: pacienteRef)
+        .get()
+        .then((value) => value.docs.map((e) => e.data()).toList());
+  }
+
+  Future<List<Agendamento>> getAllAgendamentosFromLocal(String localID) async {
+    var agendamentosConverter = agendamentosCollectionRef.withConverter(
+        fromFirestore: (snapshot, _) => Agendamento.fromDocument(snapshot),
+        toFirestore: (Agendamento agendamento, _) => agendamento.toJson());
+
+    return agendamentosConverter
+        .where("local", isEqualTo: locaisCollectionRef.doc(localID))
+        .get()
+        .then((value) => value.docs.map((e) => e.data()).toList());
+  }
+
+  Future<List<Especialidade>> getAllEspecialidades() async {
+    return especialidadesConverter
+        .orderBy("nome")
+        .get()
+        .then((value) => value.docs.map((e) => e.data()).toList());
+  }
+
+  Future<Especialidade> getEspecialidadeDetails(String especialidadeID) async {
+    return especialidadesConverter
+        .doc(especialidadeID)
+        .get()
+        .then((value) => value.data()!);
+  }
+
+  Future<Local> getLocal(String localID) async {
+    return locaisConverter.doc(localID).get().then((value) => value.data()!);
+    //     // TODO: Obter dados e gerar model pra cada especialidade
+    //     return value.data();
+    // });
+  }
+
+  Future<List<Local>> getAllLocais() async {
+    return locaisConverter
+        .get()
+        .then((value) => (value.docs.map((e) => e.data()).toList()));
+  }
+
+  Future<List<Local>> getAllLocaisWithEspecialidade(
+      String especialidadeID) async {
+    return locaisConverter
+        .where("exames",
+            arrayContains: especialidadesCollectionRef.doc(especialidadeID))
+        .get()
+        .then((value) => value.docs.map((e) => e.data()).toList());
+  }
+
+  //TODO : Criar agendamento e inserir no banco de dados...
+  Future<dynamic> createAgendamento(Agendamento newAgendamento) async {
+    // TODO: Depois de inserido no banco de dados, notificar o CacheService pra manter informações sobre esse agendamento.
+    return agendamentosCollectionRef.add(newAgendamento.toJson());
+  }
+
+  Future<void> cancelAgendamento(String agendamentoID) async {
+    // TODO: Checar se o agendamento realmente existe antes de tentar deletar. Throw erro se não existir.
+    return agendamentosCollectionRef.doc(agendamentoID).delete();
+  }
 }
