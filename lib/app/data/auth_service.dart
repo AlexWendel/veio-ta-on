eafaf37a41e4dc575521448895fcb0f07729deb7
Alex <alexwendel11@gmail.com>
2022-04-26 12:44:26 -0300

    registro, login e recovery implementados

--- lib/app/data/auth_service.dart
+++ lib/app/data/auth_service.dart
@@ -6,6 +6,7 @@ import 'package:hospital_maraba/app/models/user.dart';
 
 class AuthService extends GetxService {
   late UserLocal currentUser;
+  late String currentUserID;
 
   @override
   void onReady() async {
@@ -14,5 +15,71 @@ class AuthService extends GetxService {
           .getUserFromFirestore(FirebaseAuth.instance.currentUser?.uid);
     }
     super.onInit();
+  }
+
+  void registerUser(
+      String email, String password, Map<String, dynamic> extraData) async {
+    try {
+      FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password)
+          .then((UserCredential userCredential) async =>
+              await _createUserData(userCredential.user!.uid));
+    } on FirebaseException catch (e) {
+      switch (e.code) {
+        case "email-already-in-use":
+          break;
+        case "invalid-email":
+          break;
+        case "operation-not-allowed":
+          break;
+        case "weak-password":
+          break;
+      }
+    }
+  }
+
+  Future<UserLocal> _createUserData(String uid) async {
+    UserLocal newUserData = UserLocal(uid: uid);
+    Get.find<DatabaseService>().userConverter.doc(uid).set(newUserData);
+    return newUserData;
+  }
+
+  Future<UserLocal> _getUserData(String uid) async {
+    return Get.find<DatabaseService>().getUserFromFirestore(uid);
+  }
+
+  void login(String email, String password) async {
+    try {
+      FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password)
+          .then((credential) async {
+        if (credential.user != null) {
+          _getUserData(credential.user!.uid)
+              .then((value) => currentUser = value);
+        }
+      });
+    } on FirebaseAuthException catch (e) {
+      switch (e.code) {
+        case "invalid-mail":
+          break;
+        case "user-disabled":
+          break;
+        case "wrong-password":
+          break;
+        case "user-not-found":
+          break;
+      }
+    }
+  }
+
+  void sendRecoveryEmail(String email) async {
+    UserLocal? foundUser =
+        await Get.find<DatabaseService>().findUserByEmail(email);
+    if (foundUser == null) {
+      // TODO: Criar exceção custom para enviar mensagem para onde for necessário.
+      throw Exception("Email não registrado.");
+    } else {
+      FirebaseAuth.instance.sendPasswordResetEmail(email: foundUser.email);
+    }
   }
 }
